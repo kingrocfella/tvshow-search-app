8bb497c4cda7b8f3fe548e385c224d93db63b677
Kingsley E. Abia <KAbia@skyebank.net>
2020-04-02 19:55:23 +0100

    added end to end tests suites

--- src/utils/index.js
+++
@@ -1,8 +0,0 @@
-import checkPropTypes from "check-prop-types";
-
-export const findClassAtrr = (component, attr) => component.find(`.${attr}`);
-
-export const findItembyId = (component, id) => component.find(id);
-
-export const checkProps = (component, expectedProps) =>
-  checkPropTypes(component.propTypes, expectedProps, "props", component.name);
